0204b4064d0b14638da4340a549ff5b52052fbd6
AlSementsov <alsementsov@gmail.com>
2019-11-05 15:29:39 +0300

    Ready

--- src/main.h
+++ src/main.h
@@ -1,4 +1,4 @@
-#define ETALON 88                                                    // указываем эталонный вес
+#define ETALON 194 //Lenovo smartphone                                                   // указываем эталонный вес
 #define CONVERTION_RATE 0.03527
 #define LOADCELL_DOUT_PIN 5
 #define LOADCELL_SCK_PIN 18
