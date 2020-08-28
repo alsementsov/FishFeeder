d3e795eec515ab76cccdb4065542bae58acd3d3b
And <alsementsov@gmail.com>
2020-08-28 23:18:41 +0300

    parse Data into JSON

--- Scripts/json request_test.py
+++ Scripts/json request_test.py
@@ -1,35 +1,36 @@
 # -*- coding: utf-8 -*-
 import requests
 import json
-url = 'http://192.168.0.110'
+url = 'http://192.168.0.111'
 
+ID=1
 #Choose command
-SW=6
+SW=2
 
 if SW==0:
-    payload = {'ID': 1,'Command':0}
+    payload = {'ID': ID,'Command':0}
 elif SW==1:
-    payload = {'ID': 1,'Command':1,'Year':2020, 'Month':6, 'Day':24, 'Hour':12, 'Minute':40}
+    payload = {'ID': ID,'Command':1,'Data':{'Year':2020, 'Month':8, 'Day':7, 'Hour':14, 'Minute':10}}
 elif SW==2:
-    payload = {'ID': 1,'Command':2,'StartHour':23,'StartMinute':15,'EndHour':23,'EndMinute':35,'Freq_day':20,'Weight_day':100}
+    payload = {'ID': ID,'Command':2,'Data':{'StartHour':9,'StartMinute':0,'EndHour':10,'EndMinute':0,'Freq_day':120,'Weight_day':720}}
 elif SW==3:
-    payload = {'ID': 255,'Command':3,'IDnew':1}
+    payload = {'ID': ID,'Command':3,'Data':{'IDnew':1}}
 elif SW==4:
-    payload = {'ID': 1,'Command':4} # Тарировка  
+    payload = {'ID': ID,'Command':4} # Тарировка  
 elif SW==5:
-    payload = {'ID': 1,'Command':5} # Калибровка
+    payload = {'ID': ID,'Command':5} # Калибровка
 elif SW==6:
-    payload = {'ID': 1,'Command':6} # Старт очистки
+    payload = {'ID': ID,'Command':6} # Старт очистки
 elif SW==7:
-    payload = {'ID': 1,'Command':7} # Стоп очистки
+    payload = {'ID': ID,'Command':7} # Стоп очистки
 elif SW==8:
-    payload = {'ID': 1,'Command':8} # Глобальный СТОП 
+    payload = {'ID': ID,'Command':8} # Глобальный СТОП 
 elif SW==9:
-    payload = {'ID': 1,'Command':9} # Отключение СТОПа - ГЛОБАЛЬНЫЙ СТАРТ
+    payload = {'ID': ID,'Command':9} # Отключение СТОПа - ГЛОБАЛЬНЫЙ СТАРТ
 elif SW==10:
-    payload = {'ID': 1,'Command':10,'DefConsump':1000,'Status':1} # Consumption  
+    payload = {'ID': ID,'Command':10,'Data':{'DefConsump':1000,'Status':1}} # Consumption  
 elif SW==11:
-    payload = {'ID': 1,'Command':11,'SSID':'ABS','PASSWORD':'13121985'} # AP
+    payload = {'ID': ID,'Command':11,'Data':{'SSID':'ABS','PASSWORD':'13121985'}} # AP
 
 
 headers = {'content-type': 'application/json'}
